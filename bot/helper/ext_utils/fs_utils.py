3a278789760dbded8ba70c70b138d59712171559
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-06-11 15:37:06 +0530

    Update fs_utils.py

--- bot/helper/ext_utils/fs_utils.py
+++ bot/helper/ext_utils/fs_utils.py
@@ -12,7 +12,7 @@ import time
 from PIL import Image
 from hachoir.parser import createParser
 from hachoir.metadata import extractMetadata
-from fsplit.filesplit import Filesplit
+#from fsplit.filesplit import Filesplit
 from bot import aria2, LOGGER, DOWNLOAD_DIR, TG_SPLIT_SIZE
 
 VIDEO_SUFFIXES = ("M4V", "MP4", "MOV", "FLV", "WMV", "3GP", "MPG", "WEBM", "MKV", "AVI")
